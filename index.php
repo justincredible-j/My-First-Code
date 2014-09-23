2df4677d2b8bcb484c277dc9ae4b9fa12db1b6eb
Jason Walsh <arizona.walsh@gmail.com>
2014-09-22 18:44:00 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/*
+	echo "Hello world";
+
+    $number = 5;
+
+    if($number > 1) {
+        echo "this is greater than one";
+    }
+*/
+?>
+
+<html>
+	<head>
+		<style type="text/css">
+            body {
+                padding: 10px;
+            }
+		</style>
+	</head>
+	<body>
+	   this is the content of my webpage
+
+       <a href="/justin.php">Justin's page</a>
+	</body>
+</html>
